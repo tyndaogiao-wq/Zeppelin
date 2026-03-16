02570c09051041c75b59f2dcf1bffd8bd04921a3
tyndaogiao-wq <tyndaogiao@gmail.com>
2026-03-16 20:25:13 +0700

    Delete dashboard directory

--- dashboard/src/components/docs/CodeBlock.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <pre class="codeblock" v-highlightjs><code :class="codeLang" v-trim-indents="trim"><slot></slot></code></pre>
-</template>
-
-<script>
-  export default {
-    props: ["codeLang", "trim"],
-  };
-</script>
--- dashboard/src/components/docs/WorkInProgress.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <h1>Work in progress</h1>
-    <p>
-      This page is a work in progress.
-    </p>
-  </div>
-</template>
